787e65208ba5b7f8232592a12e8d14a0a250534f
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-19 20:55:24 +0800

    update

--- backend/src/main/java/com/avensys/SocialMediaWebApplication/role/Role.java
+++ backend/src/main/java/com/avensys/SocialMediaWebApplication/role/Role.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Role {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false, length = 10)
